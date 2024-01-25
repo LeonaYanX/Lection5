a1ce224364db1074c1d7cee13e91929cb6440ffb
LeonaYanX <149185088+LeonaYanX@users.noreply.github.com>
2024-01-25 19:40:20 +0400

    FactorialN

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿//Задача 1: Вычеслить Факториал от натурального числа Н. Ели 1! и 0! равно 1.
+int FactorialN(int N) 
+{
+    int Factorial = 0;
+    if (N <=1) 
+    {
+        Factorial = 1;
+        return Factorial;
+    }
+    return Factorial= N * FactorialN(N - 1);
+}
+Console.WriteLine(FactorialN(3));
+Console.ReadLine();
