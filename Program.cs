1b72e1d911665069442f56b574e54daf9d00bf85
LeonaYanX <149185088+LeonaYanX@users.noreply.github.com>
2024-01-25 20:19:14 +0400

    UNWINDING

--- Program.cs
+++ Program.cs
@@ -1,13 +1,24 @@
 ﻿//Задача 1: Вычеслить Факториал от натурального числа Н. Ели 1! и 0! равно 1.
 int FactorialN(int N) 
 {
+    
     int Factorial = 0;
     if (N <=1) 
     {
+        Console.WriteLine($"Stoping reqursion N = {N}");
         Factorial = 1;
+        Console.WriteLine($"Factorial N = {Factorial}");
         return Factorial;
     }
-    return Factorial= N * FactorialN(N - 1);
+    Console.WriteLine($"N is = {N}");
+    Console.WriteLine($"Factorial N = {Factorial}");
+    Factorial = N * FactorialN(N - 1);
+    Console.WriteLine($"Unwinding N ={N}");
+    Console.WriteLine($" UNWINDING Factorial N = {Factorial}");
+    return Factorial;
 }
-Console.WriteLine(FactorialN(3));
+Console.WriteLine(FactorialN(5));
+
 Console.ReadLine();
+
+
